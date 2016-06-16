3185eb052624e2ea371f3ffa54bdfdbd4edfa9f7
Isaac Whitfield <iwhitfield@appcelerator.com>
2016-06-16 14:08:21 +0100

    Bump to v1.0.1

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Eternal.Mixfile do
         },
         maintainers: [ "Isaac Whitfield" ]
       },
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.1",
       deps: deps,
       docs: [
